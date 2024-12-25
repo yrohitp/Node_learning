be77763fc2e148febad1fb7aa00eafe76d1b805d
yrohitp <yrohitp@gmail.com>
2024-12-25 07:24:44 +0530

    learning with file system

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const http = require("http");
 
+const fs = require("fs");
+
 const myserver = http.createServer((req,res)=>{
+    const log = `${Date.now()}; New Req Recieved\n`;
+    fs.appendFile("log.txt",log,(err,data) => {
+        res.end('Hello fronm server Again')
+    }); 
+
     console.log("server request is comming");
     res.end("hello from server");
 });
 
-myserver.listen(8000,() => console.log("server is running"));
+myserver.listen(8000,() => console.log("server is running on 8000"));
